b795374829702255bd3fa9ca8cd96aa5f27cacae
张三 <zhangsan@itcast.cn>
2020-11-28 11:48:39 +0800

    把本地工程添加到git3

--- OA/src/main/java/enity/user.java
+++ OA/src/main/java/enity/user.java
@@ -1,15 +1,14 @@
 package enity;
 
-/**
- * @Author: ws
- * @Date: 2020/11/28 0028 11:24
- * @Version 1.0
- */
 public class user {
     private String username;
     private String password;
     public void test(){
 
         int i = 10;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("helloworld");
     }
 }
